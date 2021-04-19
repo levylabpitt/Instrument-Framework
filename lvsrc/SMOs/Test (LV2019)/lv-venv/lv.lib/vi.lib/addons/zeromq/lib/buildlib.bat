3ce59be3e84b93c618796f3559912fd78422d79d
Patrick Irvin <p.irvin@levylab.org>
2021-04-19 09:36:48 -0400

    delete Test (LV2019)

--- lvsrc/SMOs/Test (LV2019)/lv-venv/lv.lib/vi.lib/addons/zeromq/lib/buildlib.bat
+++
@@ -1,5 +0,0 @@
-REM Build script for zeromq-4.1.x
-cl /nologo /LD /Felibzmq%ARCH%.dll /DDLL_EXPORT /DNDEBUG /DZMQ_USE_SELECT /DFD_SETSIZE=1024 /EHsc /O2 /Ox /Ibuilds\msvc src/*.cpp builds\msvc\errno.cpp /link advapi32.lib ws2_32.lib iphlpapi.lib
-IF %ERRORLEVEL% NEQ 0 GOTO end
-del *.obj
-:end
